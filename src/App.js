d4f6598a8213b360b20e01ef72716af625c25318
Matheus De Assis <65235458+MatheusAssisM@users.noreply.github.com>
2021-03-10 14:42:37 -0300

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,19 +1,19 @@
-import React, {useState} from "react";
+import React, { useState } from "react";
 import api from "./api";
 
 function App() {
   const [pokemonInfo, setPokemonInfo] = useState([]);
 
 
-  const getPokemon = async () =>{
+  const getPokemon = async () => {
     const list = [];
-    const response = api.get("pokemon/pikachu");
+    const response = await api.get("pokemon/pikachu");
     list.push(response.data);
     setPokemonInfo(list);
   }
 
   getPokemon();
-  
+
   return (
     <div>
       {/* <h1>Insira seu pokemon</h1>
